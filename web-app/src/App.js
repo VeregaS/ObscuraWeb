da0ea5993be9fd72a740da5ee1a4b304ef0b6932
VeregaS <sergeiusenkov56@gmail.com>
2025-01-31 19:42:59 +0300

    vA01

---
+++ web-app/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", fontSize: "24px" }}>
+      Добро пожаловать в Web App!
+    </div>
+  );
+}
+
+export default App;
