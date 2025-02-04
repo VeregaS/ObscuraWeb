a3ee53665f1303e5f12978cb29dcadc369cd2217
VeregaS <sergeiusenkov56@gmail.com>
2025-02-04 20:27:31 +0500

    .

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {Suspense} from "react";
+import { Helmet } from 'react-helmet';
 import styles from "./App.module.css"
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <div className={styles.version}>
-      Добро пожаловать в Web App!
+    <div>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <div className={styles.version}>ObscuraWeb v0.1a</div>
+      <Suspense>
+        {<NavBar is_visible={true}/>}
+      </Suspense>
     </div>
   );
 }
